a6c59a844217a36a1e4cf718ee689774f48bbfa4
Apprentice <apprentice@devbootcamp.com>
2013-06-15 18:07:39 -0700

    create event and add item are working in javascript.

--- PotluckyGopher/app/assets/javascripts/events.js
+++ PotluckyGopher/app/assets/javascripts/events.js
@@ -7,78 +7,113 @@
 // t.integer :quantity_provided
 //----------------------------Event related  functions ----------------------------//
 function Item(name, suggestion, quantity){
-  this.id =
-  this.name = name;
-  this.suggestion = suggestion;
-  this.quantity_needed = quantity;
+  this.name                  = name;
+  this.suggestion            = suggestion;
+  this.quantityNeeded        = quantity;
+  this.eventsItemsTemplate   = '<div class="events_items"><div class="name">'+ this.name +'</div><div class="suggestion">'+ this.suggestion +'</div><div class="quantity_needed">' + this.quantityNeeded +'</div><div>';  
 }
 
+Item.prototype = {
+      constructor: Item,
+  updateItemDetails: function(name, suggestion, quantity){
+                      this.name                   = name;
+                      this.suggestion             = suggestion;
+                      this.quantityNeeded         = quantity;
+                    },
+    addGuestDetails: function(quantityProvided, guestEmail, guestName){
+                       this.quantityProvided      = quantity_provided;
+                       this.guestEmail            = guestEmail;
+                       this.guestName             = guestName;
+                       this.assignedItemsTemplate = '<div class="assigned_item"><div class="name">'+ this.name +'</div><div class="guest_name">'+ this.guestName + '</div><div class="guest_email">'+ this.guestEmail + '</div><div class="quantity_provided">' + this.quantityProvided + '</div><div>';
+                    }
+};
 
-
-function Event(eventName, place, date, description){
-  this.eventName = eventName;
-  this.desc = description;
-  this.date = date;
-  this.place = place;
-  this.items = []
+function Event(){
+  this.items     = [];
 }
 
-Event.prototype.add = function(item){
-  this.items.push(item);
+Event.prototype = {
+  constructor: Event,
+   addDetails: function(eventName, place, date, description){
+                  this.eventName = eventName;
+                  this.desc      = description;
+                  this.date      = date;
+                  this.place     = place;
+                  this.template  = '<div class="event_form"><div class="name" placeholder="Title">'+ this.eventName +
+                   '</div><div class ="place" placeholder="Location">'+ this.place +
+                   '</div><div type="datetime" class="date" placeholder="Date">'+ this.date +
+                   '</div><div class="description" placeholder="Description">'+ this.desc +
+                   '</div></div>';
+                },
+           add: function(item){
+                  this.items.push(item);
+                }
 };
 //----------------------------Rendering functions ----------------------------//
-function createEvent(){
-  $('.add_event').on('click', function(){
-    var eventName = $('.event_name').val();
-    var place = $('.place').val();
-    var date = $('.date').val();
-    var description = $('.description').val();
-    myEvent = new Event(eventName, place, date, description);
-    // console.log(myEvent);
-    renderEvent(myEvent);
-    $('.items_form').css('display', '');
-  });
+var EventRenderer = {
+          render: function(newEvent){
+                    console.log(newEvent);
+                    $('.event_form').hide();
+                    $('.event_details').append(newEvent.template);
+                  },
+      submitForm: function(newEvent){
+                    var eventName = $('.event_name').val();
+                    var place = $('.place').val();
+                    var date = $('.date').val();
+                    var description = $('.description').val();
+                    newEvent.addDetails(eventName, place, date, description);
+                    this.render(newEvent);
+                    $('.items_form').css('display', '');
+                  },
+
+ displayAllItems: function(newEvent){
+                    newEvent.items.forEach(ItemRenderer.render);
+                  }
 };
 
-function createItem(createdEvent){
-  $('.add_item').on('click', function(){
-    var name = $('.item_name').val();
-    var sug = $('.suggestion').val();
-    var quantity = $('.quantity_needed').val();
-    var item = new Item(name, sug, quantity);
-    console.log(item);
-    createdEvent.add(item);
-    createdEvent.renderEventsItems();
-  });
-}
-
-function renderEvent(myEvent){
-  eventTemplate = '<div class="event_form"><div class="name" placeholder="Title">'+ myEvent.eventName +'</div><div class ="place" placeholder="Location">'+ myEvent.place +'</div><div type="datetime" class="date" placeholder="Date">'+ myEvent.date +'</div><div class="description" placeholder="Description">'+ myEvent.desc +'</div></div>';
-  $('.event_form').hide();
-  $('.event_details').append(eventTemplate);
+var ItemRenderer = {
+  createItem: function(){
+                var name = $('.item_name').val();
+                var sug = $('.suggestion').val();
+                var quantity = $('.quantity_needed').val();
+                var newItem = new Item(name, sug, quantity);
+                return newItem;
+              },
+      render: function(element, index, array){
+                $('.added_items').append(element.eventsItemsTemplate);//.fadeIn(800);
+              }
+  // $('.add_item').on('click', function(){
+  //   var name = $('.item_name').val();
+  //   var sug = $('.suggestion').val();
+  //   var quantity = $('.quantity_needed').val();
+  //   var item = new Item(name, sug, quantity);
+  //   console.log(item);
+  //   createdEvent.add(item);
+  //   createdEvent.renderEventsItems();
+  // });
 };
 
-Event.prototype.renderEventsItems = function(){
-  console.log("RENDER MY EVENT ITEMS DAMNIT");
-  console.log(this.items)
-  this.items.forEach(renderItem);
+// function render(){
+//   this.
+// }
+
+function NewEventForm(){
+    var myEvent        = new Event();
+    var addEventNode   = $('.add_event');
+    var addItemNode    = $('.add_item');
+    var eventItemsNode = $('.added_items');
+
+    addEventNode.on('click', function(){
+      EventRenderer.submitForm(myEvent);
+    });
+
+    addItemNode.on('click', function(){
+      createdItem = ItemRenderer.createItem();
+      myEvent.add(createdItem);
+      ItemRenderer.render(createdItem);
+    });
 }
-
-
-function renderItem(element){
-  console.log(element)
-  var itemTemplate = '<div class="assigned_item"><div class="name">'+ element.name +'</div><div class="suggestion">'+ element.suggestion +'</div><div class="quantity">' + element.quantity_needed + '</div><div>';
-  $('.added_items').append(itemTemplate);
-}
-
-var renderCreateEventForm = {
-  init: function(){
-    createEvent();
-  }
-}
-
 
 $(function(){
-  // var myEvent = new Event;
-  renderCreateEventForm.init();
+  NewEventForm();
 });
