1e503df1a1a2d79847c8b5fe796313a717e692e2
Apprentice <apprentice@dbc09.local>
2013-06-16 21:50:18 -0700

    style changes for events#show and assigned item model created

--- PotluckyGopher/app/models/event_item.rb
+++ PotluckyGopher/app/models/event_item.rb
@@ -1,5 +1,15 @@
 class EventItem < ActiveRecord::Base
   attr_accessible :event_id, :description, :item_id, :quantity_needed
+  
+  # validates :quantity_needed, :presence => true, :message => "Please tell your guest how much you need."
+  # validates :quantity_needed, :numericality => { 
+  #   :only_integer => true 
+  # }
+  # validates :description, :length => {
+  #   :maximum => 140,
+  #   :too_long => "must be shorted thatn 140 characters."
+  # }
+
   has_many :assigned_items
   belongs_to :event 
   belongs_to :item 
