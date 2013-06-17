86d3123783872d8a6aab1ed593e3a63247bbd789
Apprentice <apprentice@dbc09.local>
2013-06-16 21:05:02 -0700

    bring item redirects to the assigned item's event.

--- PotluckyGopher/app/controllers/assigned_items_controller.rb
+++ PotluckyGopher/app/controllers/assigned_items_controller.rb
@@ -1,13 +1,12 @@
 class AssignedItemsController < ApplicationController
 
 def create
-  p @event
-  p params
   @assigned_item = AssignedItem.new(params[:assigned_item])
+  @event = Event.find(EventItem.find(@assigned_item.event_item_id).event_id)
   if @assigned_item.save
-    p "worked!!!!!!!!!!!!!!!!!!!!!!"
+    redirect_to event_path(@event)
   else
-    p "didn't :((((((((((:((((((((((:((((((((((:(((((((((("
+    p "didn't :((((((((((:((((((((((:((((((((((:((((((((((" #obviously need to write errors for this
   end
 end
 
--- PotluckyGopher/app/controllers/events_controller.rb
+++ PotluckyGopher/app/controllers/events_controller.rb
@@ -18,9 +18,6 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.create(params[:event], url: SecureRandom.urlsafe_base64, user_id: current_user.id)
-    # items.each do |i|
-    #   @event.items << EventItem.create(name: items[i[0]]["name"] , suggestion: items[i[0]]["suggestion"], quantity_needed: items[i[0]]["quantityNeeded"])
-    # end
     if @event.save
       redirect_to edit_event_path(@event)
     else
