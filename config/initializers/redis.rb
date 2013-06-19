1758c4acc91f998ced01746df6c6c31adb14204d
Amy Lukima <amylukima@gmail.com>
2013-06-19 12:17:55 -0700

    remove unused config file

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,10 @@
-$redis = Redis.connect :url => ENV["OPENREDIS_URL"]
+if ENV.has_key?("REDISTOGO_URL")
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+elsif Rails.env.test?
+  Rails.logger.warn "Using fake redis..."
+  $redis = FakeRedis::Redis.new
+else
+  Rails.logger.warn "Using local redis daemon"
+  $redis = Redis.new
+end
