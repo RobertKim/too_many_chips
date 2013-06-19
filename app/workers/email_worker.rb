aa52774ab90d2195b14f85bb766c048a39b23356
Amy Lukima <amylukima@gmail.com>
2013-06-19 12:37:06 -0700

    correct method name from registraion to subscription_confirmation in mailers

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -6,7 +6,7 @@ class EmailWorker
     puts "*" * 100
     puts user_id    
    #if email_type == "signup"
-    UserMailer.registration(user_id).deliver
+    UserMailer.signup_confirmation(user_id).deliver
 
     # elsif email_type == "confirm"
     #   UserMailer.confirmation(user_id, event_id).deliver
