a9fd2af5b84b99a4710680ce6e22a7673c4c7a82
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-06-19 18:34:00 -0700

    got the show form working with ajax to display errors and not create assigned items with 0 quantity provided

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,11 +1,23 @@
 class GuestsController < ApplicationController
+  def new
+    @guest = Guest.new
+    @guest.assigned_items.build
+    @assigned_item = AssignedItem.new
+  end
 
   def create
-    @guest = Guest.new(params[:guest])    
+    @guest = Guest.new(params[:guest])
+    
     if @guest.save
-      redirect_to root_path  
+      # @assigned_items = @guest.assigned_items
+    #   @items = 
+    #   items = @assigned_items.each do |s|
+    #     @assigned_items.event_item.item.name)
+      render :json => root_path.to_json   
+    #   render :json => @guest.assigned_items
+    # end
     else
-      render :new
+      render :json => @guest.errors.full_messages.join(','), :status => :unprocessable_entity      
     end
   end
 
@@ -13,11 +25,7 @@ class GuestsController < ApplicationController
 
   end
 
-  def new
-    @guest = Guest.new
-    @guest.assigned_items.build
-    @assigned_item = AssignedItem.new
-  end
+  
 
   def show
     @guest = Guest.find(params[:guest])
--- app/models/assigned_item.rb
+++ app/models/assigned_item.rb
@@ -1,13 +1,17 @@
 class AssignedItem < ActiveRecord::Base
   attr_accessible :event_item_id, :quantity_provided, :guest_id
   validates :quantity_provided, :presence => true
-  validates :quantity_provided, :numericality => {:only_integer => true, :greater_than => 0}
+  validates :quantity_provided, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
   belongs_to :event_item
   belongs_to :guest
   has_many :assigned_items, :through => :event_items
 
+  after_create :delete_if_zero
+
   def find_all_user_items #this name isn't very descriptive.
     AssignedItem.where('guest_email=?', self.guest_email)
   end
-
+  def delete_if_zero
+    self.destroy if self.quantity_provided == 0
+  end
 end
